0933c4d2db3b70e4fcc1e2f2f452732cb790418e
Erik Van Kelst <4levels@gmail.com>
2019-07-18 17:39:55 +0200

    Try wkhtmltopdf on darwin..

--- pkgs/tools/graphics/wkhtmltopdf/default.nix
+++ pkgs/tools/graphics/wkhtmltopdf/default.nix
@@ -141,6 +141,6 @@ stdenv.mkDerivation rec {
     '';
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ jb55 ];
-    platforms = with platforms; linux;
+    platforms = platforms.linux ++ platforms.darwin; #with platforms; linux;
   };
 }
